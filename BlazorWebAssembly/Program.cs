1bef4718a49f7031b727f4ad05850297ce0bb819
Seloy <nsel@itu.dk>
2022-11-23 11:55:04 +0100

    added a button to log in and setup inital

--- BlazorWebAssembly/Program.cs
+++ BlazorWebAssembly/Program.cs
@@ -6,9 +6,12 @@ using System.Net.Http.Json;
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
 
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
